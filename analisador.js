8134ea0c9db6c15c27ea585fcb39c85750bbe052
Renar Freitas dos Santos <38799859+renarfreitas@users.noreply.github.com>
2020-04-08 23:13:22 -0300

    Continuação do exercicio 07

--- analisador.js
+++ analisador.js
@@ -1,1 +1,28 @@
-alert('Òla!')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumber(n) {
+  if(Number(n) >= 1 && Number(n) <=100) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function inListen(n, l){
+  if (l.indexOf(Number(n)) != -1) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function adicionar(){
+  if(isNumber(num.value) && !inListen(num.value, valores)) {
+    window.alert('Tudo Ok!')
+  } else {
+    window.alert('Valor inválido ou já encontrado na lisat.')
+  }
+}
